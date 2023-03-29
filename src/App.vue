66621b2054738f6434044442d79497f4b95f6369
Hugo andres torres <hugotorreslinares@gmail.com>
2023-03-29 19:25:26 +0000

    better route

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="filter" placeholder="Filter by name" />
+    <crime-statistics :localities="localityData" :filter="filter" />
+  </div>
+</template>
+
+<script>
+import CrimeStatistics from '@/components/CrimeStatistics.vue';
+import localityData from "@/data/localityData.json";
+
+export default {
+  name: 'App',
+  components: {
+    CrimeStatistics
+  },
+  data() {
+    return {
+      localityData,
+      filter: ""
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CrimeStatistics.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h2>Crime Statistics by Locality in Bogotá</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Locality</th>
+          <th>Population (2021)</th>
+          <th>Reported Crimes (2020)</th>
+          <th>Crime Rate (per 100,000 people)</th>
+          <th>Crime Rate per Person</th>
+          <th>Area (km²)</th>
+          <th>Number of Police Stations</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="locality in filteredData" :key="locality.id">
+          <td>{{ locality.name }}</td>
+          <td>{{ locality.population }}</td>
+          <td>{{ locality.reportedCrimes }}</td>
+          <td>{{ locality.crimeRate }}</td>
+          <td>{{ locality.crimeRatePerPerson }}</td>
+          <td>{{ locality.area }}</td>
+          <td>{{ locality.numPoliceStations }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CrimeStatistics",
+  props: {
+    filter: String,
+    localities: Array,
+  },
+  data() {
+  },
+  computed: {
+    filteredData() {
+      const regex = new RegExp(this.filter, "i");
+      return this.localities.filter((locality) => regex.test(locality.name));
+    },
+  },
+};
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th {
+  background-color: #ddd;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+</style>
